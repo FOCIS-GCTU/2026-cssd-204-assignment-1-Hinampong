ca459b50048375ddd68500cbe4cf29ea28f1001f
Hinampong <2425402885@live.gctu.edu.gh>
2026-05-31 10:26:12 +0000

    Add files via upload
    A commit for my initials.py assignment submission

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: initails.py
+# Description: Print out initails in stylized large letters
+# Assignment: 1
+
+# Name: Hinampong John
+# Student ID: 2425402885
+# Email: 2425402885@live.gctu.edu.gh
+# Grader: <Carolyn OR Emma OR Ahmad>
+
+# On my honor, Hinampong John, this programming assignment is my own work
+# and I have not provided his code to any other student.
+
 
 def main():
-  pass #code goes here
-
-
+    # Printing out my initials H, J as 12 wide by 10 tall block letters using only strings and print.
+    print()
+    print("...HJ")
+    print()
+    print("...HHH......HHH........JJJJJJJJJJJJ.......................")
+    print("...HHH......HHH.................JJJ.......................")
+    print("...HHH......HHH.................JJJ.......................")
+    print("...HHH......HHH.................JJJ.......................")
+    print("...HHHHHHHHHHHH.................JJJ.......................")
+    print("...HHHHHHHHHHHH.................JJJ.......................")
+    print("...HHH......HHH.................JJJ.......................")
+    print("...HHH......HHH........JJJ......JJJ.......................")
+    print("...HHH......HHH...**...JJJJJ....JJJ...**..................")
+    print("...HHH......HHH...**...JJJJJJJJJJJJ...**..................")
+    print()
 
 main()
+    
