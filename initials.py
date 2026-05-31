9563885a322b86982cb3ab43b162bb88ff51bb29
Hinampong <2425402885@live.gctu.edu.gh>
2026-05-31 13:57:56 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,32 +1,34 @@
 # File: initials.py
-# Description: Print out initails in stylized large letters
+# Description: Print out initials in stylized large letters
 # Assignment: 1
 
-# Name: Hinampong John
+# Name: Hinampong Kofi John
 # Student ID: 2425402885
 # Email: 2425402885@live.gctu.edu.gh
 # Grader: <Carolyn OR Emma OR Ahmad>
 
-# On my honor, Hinampong John, this programming assignment is my own work
-# and I have not provided his code to any other student.
+# On my honor, Hinampong Kofi John, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 
 def main():
-    # Printing out my initials H, J as 12 wide by 10 tall block letters using only strings and print.
+    # Printing out my initials H, K, J as block letters using only strings and print.
     print()
-    print("...HJ")
-    print()
-    print("...HHH......HHH........JJJJJJJJJJJJ.......................")
-    print("...HHH......HHH.................JJJ.......................")
-    print("...HHH......HHH.................JJJ.......................")
-    print("...HHH......HHH.................JJJ.......................")
-    print("...HHHHHHHHHHHH.................JJJ.......................")
-    print("...HHHHHHHHHHHH.................JJJ.......................")
-    print("...HHH......HHH.................JJJ.......................")
-    print("...HHH......HHH........JJJ......JJJ.......................")
-    print("...HHH......HHH...**...JJJJJ....JJJ...**..................")
-    print("...HHH......HHH...**...JJJJJJJJJJJJ...**..................")
+    print("...HKJ")
     print()
 
+    print("...HHH......HHH........KKK........KKK........JJJJJJJJJJJJ.......................")
+    print("...HHH......HHH........KKK......KKK................JJJ.........................")
+    print("...HHH......HHH........KKK....KKK.................JJJ.........................")
+    print("...HHH......HHH........KKK..KKK...................JJJ.........................")
+    print("...HHHHHHHHHHHH........KKKKKK.....................JJJ.........................")
+    print("...HHHHHHHHHHHH........KKKKKK.....................JJJ.........................")
+    print("...HHH......HHH........KKK..KKK...................JJJ.........................")
+    print("...HHH......HHH........KKK....KKK.........JJJ.....JJJ.........................")
+    print("...HHH......HHH...**...KKK......KKK...**...JJJJJ....JJJ...**..................")
+    print("...HHH......HHH...**...KKK........KKK..**....JJJJJJJJJJ...**..................")
+
+    print()
+
+
 main()
-    
